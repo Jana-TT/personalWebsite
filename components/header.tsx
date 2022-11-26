4f39a718faff6db676e6596e4f1c38b4a60e0a92
Jana-TT <112980645+Jana-TT@users.noreply.github.com>
2022-11-26 16:42:56 -0600

    components added

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,11 @@
-import Link from "next/link";
+import ButtonHeader from "./buttonheader";
 
 export default function Header(){
     return(
     <div className='pt-4 bg-gray-100'>
         <div className="space-x-3 ">
-            <Link href='/resume'>
-                <button className="bg-sky-100 hover:bg-sky-200 text-white font-bold py-2 px-4 border border-sky-100 rounded">Resume</button>
-            </Link>
-
-            <Link href='/gamedevelopment'>
-                <button className="bg-sky-100 hover:bg-sky-200 text-white font-bold py-2 px-4 border border-sky-100 rounded">Game Development</button>
-            </Link>
+            <ButtonHeader pageLink="/resume" buttonName="Resume"/>
+            <ButtonHeader pageLink="/gamedevelopment" buttonName="Game Development"/>
         </div>
     </div>
 
