23e400307eb7ae0f7ebfa5e19914679973084483
Jana-TT <112980645+Jana-TT@users.noreply.github.com>
2022-11-27 16:41:37 -0600

    added card component

--- components/card.tsx
+++ components/card.tsx
@@ -9,13 +9,11 @@ export default function Card({pageLink, title, content} : props) {
         <a href={pageLink} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-800 hover:outline hover:outline-purple hover:outline-4">
             <h5 className="mb-2 text-lg font-bold text-gray-900 dark:text-white">{title}</h5>
             <p className="font-normal text-gray-700 dark:text-gray-400"></p>
-            {content.map(e => {
-            <li>
-                {e}
-            </li>
-           })
-}
-
+            {content.map(e => 
+                <ul>
+                    {e}
+                </ul>
+            )}
         </a>
     )
 }
--- components/skills.tsx
+++ components/skills.tsx
@@ -2,8 +2,18 @@ import Card from "./card";
 
 export default function Skills() {
     return(
-        <div className="pt-14 pl-96 space-x-3">
-            <Card pageLink='/resume' title='Skills'/>
+        <div className="pt-14 pl-96">
+            <div className="space-x-3">
+                <Card pageLink='/resume' title='Skills' content={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
+                "Game Engine: Unity", "Databases: NoSQL (MongoDb), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]}/>
+
+                <Card pageLink='' title='Projects' content={[""]}/> 
+
+            </div>
+            
+        
+        
+        
         </div>
         
        
