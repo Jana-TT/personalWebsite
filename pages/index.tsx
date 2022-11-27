a1726257fa03e0a9de4910c86a6f58a279f8922c
Jana <janatt2056@gmail.com>
2022-11-27 10:56:26 -0600

    fixing cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import BigName from '../components/bigname'
 import Header from '../components/header'
 import Skills from '../components/skills'
 
+
 export default function Home() {
   return (
+   
     <div className='h-screen'>
 
         <Header/>
         <BigName/>
         <Skills/>
+
+        
 
     </div>
   )
