d0104ffcc7ca4fc0d6989ad1c6e3bbeb3b7423f5
Jana <janatt2056@gmail.com>
2022-11-27 22:25:15 -0600

    add

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,7 +4,7 @@ export default function Skills() {
     return(
         <div className="flex items-stretch pl-96 ml-24 mt-2">
 
-            <div className="py-4 mt-4 hover:p-8">
+            <div className="py-4 pl-4 mt-4 hover:p-8">
                 <Card pageLink='/resume' title='Skills' content={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
                 "Game Engine: Unity", "Databases: NoSQL (MongoDb), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]}/> 
             </div>
@@ -16,8 +16,7 @@ export default function Skills() {
             </div>
         
             <div className="py-4 mt-4 pl-4 hover:p-8">
-                <Card pageLink='' title='About Me!' content={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
-                "Game Engine: Unity", "Databases: NoSQL (MongoDb), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]}/> 
+                <Card pageLink='' title='About Me!' content={["always doing something wrong"]}/> 
             </div>
         
         </div>
