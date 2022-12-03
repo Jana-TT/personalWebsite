f2780ba88f24c657ee89ee384a8970bc1fabfc7d
Jana-TT <112980645+Jana-TT@users.noreply.github.com>
2022-12-03 16:51:32 -0600

    justifyCenter

--- components/skills.tsx
+++ components/skills.tsx
@@ -2,23 +2,14 @@ import Card from "./card";
 
 export default function Skills() {
     return(
-        <div className="flex items-stretch pl-96 ml-24 mt-2">
-
-            <div className="py-4 pl-4 mt-4 hover:p-8">
-                <Card pageLink='/resume' title='Skills' content={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
-                "Game Engine: Unity", "Databases: NoSQL (MongoDb), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]}/> 
-            </div>
-
-            <div className="py-8 pl-4 hover:p-8">
-                <Card pageLink='' title='Projects' content={["Arcade Game: HullBreakers", "Reservation System: OpenTable", 
+        <div className="object-center flex items-stretch justify-center space-x-3 mt-6">
+            <Card pageLink='/resume' title='Skills' content={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
+            "Game Engine: Unity", "Databases: NoSQL (MongoDb), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]}/> 
+            
+            <Card pageLink='' title='Projects' content={["Arcade Game: HullBreakers", "Reservation System: OpenTable", 
                 "Game: Butterslide", "Personal Portfolio: this website"]}
                 />
-            </div>
-        
-            <div className="py-4 mt-4 pl-4 hover:p-8">
-                <Card pageLink='' title='About Me!' content={["always doing something wrong"]}/> 
-            </div>
-        
+            <Card pageLink='' title='About Me!' content={["always doing something wrong"]}/> 
         </div>
         
        
