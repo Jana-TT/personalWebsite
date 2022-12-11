5b1f1b37538dd1b4f097341b4fdc1d1cccad610b
Jana <janatt2056@gmail.com>
2022-12-10 19:31:44 -0600

    changing stuff

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -8,17 +8,6 @@ export default function Resume(){
             <HeaderInfo header="Education"/>
             <ResumeInfo info={["University of Houston", "B.S. Computer Science", "Minor - Mathematics", "August 2019 - December 2022", "Awards: Dean's List - Fall 2019, Dean's Distinguished Scholars - Spring 2021"]}/>
             
-            <div className="pt-3">
-                <HeaderInfo header="Skills"/>
-                <ResumeInfo info={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
-                "Game Engine: Unity", "Databases: NoSQL (MongoDb), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]}/>
-            </div>
-
-            <div className="pt-3">
-                <HeaderInfo header="Work Experience"/>
-                <ResumeInfo info={["Video Game Desginer", "SmashShip Studios"]}/>
-                <ResumeInfo info={["Lead video game desginer", "Collaborated with lead developer and artists on design and delivery of interactive application"]}/>
-            </div>
         </div>
     )
 }
