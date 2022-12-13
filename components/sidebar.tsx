521c172f135dc9eabe5d942259f69e5189488380
Jana <janatt2056@gmail.com>
2022-12-12 21:21:23 -0600

    plzwork

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,5 @@
 export default function SideBar() {
     return(
-        <div className="w-28 bg-gradient-to-t from-galaxy-500 to-galaxy-400 rounded"/>
+        <div className="w-28 h-full bg-gradient-to-t from-galaxy-500 to-galaxy-400 rounded"/>
     )
 }
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,24 +4,19 @@ import ResumeInfo from "../components/resume/resumeinfo"
 import SideBar from "../components/sidebar"
 
 export default function Resume(){
-    return(
-        <div className = "h-screen flex">
+    return(        
+        <>
+        <div className="bg-gradient-to-t from-galaxy-500 to-galaxy-400 h-screen w-28 absolute" />
+        <div className="pl-36 pt-2">
+            <HeaderInfo header="Education" />
+            <ResumeInfo info={["University of Houston", "B.S. Computer Science", "Minor - Mathematics", "August 2019 - December 2022", "Awards:", " Dean's List - Fall 2019", " Dean's Distinguished Scholars - Spring 2021"]} subcontent={[]} />
 
-            <SideBar/>
-          
-            <div className="pl-8 pt-2 w-5/6">
-                <HeaderInfo header="Education"/>
-                <ResumeInfo info={["University of Houston", "B.S. Computer Science", "Minor - Mathematics", "August 2019 - December 2022", "Awards:", "** Dean's List - Fall 2019", "** Dean's Distinguished Scholars - Spring 2021"]} subcontent={[]}/>
+            <HeaderInfo header="Skills"/>
+            <ResumeInfo info={["Languages: JavaScript, C++, Python", "Front-End Framework: React", "Game Engine: Unity", "Databases: NoSQL (MongoDB), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]} subcontent={[]}/>
 
-                <HeaderInfo header="Skills"/>
-                <ResumeInfo info={["Languages: JavaScript, C++, Python", "Front-End Framework: React", "Game Engine: Unity", "Databases: NoSQL (MongoDB), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]} subcontent={[]}/>
-
-                <HeaderInfo header="Work Experience"/>
-                <ResumeInfo info={["Lead game art designer", "Created the 2D visual aspects of the game", "Collaborated with developers and artists on design and delivery of interactive applications"]} subcontent={["Game Art Designer", "SmashShip Studios"]}/>
-
-
-            </div>
-
+            <HeaderInfo header="Work Experience"/>
+            <ResumeInfo info={["Lead game art designer", "Created the 2D visual aspects of the game", "Collaborated with developers and artists on design and delivery of interactive applications"]} subcontent={["Game Art Designer", "SmashShip Studios"]}/>
         </div>
+        </>
     )
 }
