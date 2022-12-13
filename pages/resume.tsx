97b17964733058ce70fc69e69173e20ff2c113de
Jana <janatt2056@gmail.com>
2022-12-12 22:18:58 -0600

    testing

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -4,24 +4,28 @@ import SideBar from "../components/sidebar"
 
 export default function Resume(){
     return(
-        <div className="h-full bg-gradient-to-t from-galaxy-500 to-galaxy-400 overflow-y">
+        <div className="ml-32 pt-4 h-screen bg-gray-300 pl-10 grid grid-cols-4 space-x-6">
         
-            <div className="ml-32 pt-4 h-full bg-gray-300 pl-10">
+
+                <div>
                 <HeaderInfo header="Education"/>
                 <ResumeInfo info={["University of Houston", "B.S. Computer Science", "Minor - Mathematics", "August 2019 - December 2022", "Awards:", " Dean's List - Fall 2019", " Dean's Distinguished Scholars - Spring 2021"]} subcontent={[]}/>
-            
+                </div>
+
+                <div>
                 <HeaderInfo header="Skills"/>
                 <ResumeInfo info={["Languages: JavaScript, C++, Python", "Front-End Framework: React", "Game Engine: Unity", "Databases: NoSQL (MongoDB), DGraph", "Source Control: Git, PlasticSCM", "Operating System: Windows"]} subcontent={[]}/>
+                </div>
 
+                <div className="">
                 <HeaderInfo header="Work Experience"/>
                 <ResumeInfo info={["Lead game art designer", "Created the 2D visual aspects of the game", "Collaborated with developers and artists on design and delivery of interactive applications"]} subcontent={["Game Art Designer", "SmashShip Studios"]}/>
-            
-                <HeaderInfo header="Work Experience"/>
                 <ResumeInfo info={["Lead game art designer", "Created the 2D visual aspects of the game", "Collaborated with developers and artists on design and delivery of interactive applications"]} subcontent={["Game Art Designer", "SmashShip Studios"]}/>
-                
+                </div>
+
             </div>
             
                         
-        </div>
+        
     )
 }
