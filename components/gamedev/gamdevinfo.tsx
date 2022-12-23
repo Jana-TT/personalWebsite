e77a71aea63222328d8ea2fa33a0ade9abec1e7b
Jana <janatt2056@gmail.com>
2022-12-23 14:54:11 -0600

    phhh

--- components/gamedev/gamdevinfo.tsx
+++ components/gamedev/gamdevinfo.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
+import HeaderInfo from "../animations/headerinfo"
 
 interface props{
     text: string[]
@@ -7,15 +8,21 @@ interface props{
     widthSize: number
     heightSize: number
     linkPage: string
+
+    header2 : string
+    d12: string
+    d22: string
+    d32: string 
 }
 
-export default function Gamedevinfo({text, imageSource, widthSize, heightSize, linkPage}: props) {
+export default function Gamedevinfo({text, imageSource, widthSize, heightSize, linkPage, header2, d12, d22, d32}: props) {
     return(
         <Link href={linkPage}>
             <div className="flex p-4 justify-center">
                 <Image src={imageSource} alt="" width={widthSize} height={heightSize}/> 
 
-                <div className="flex-none pl-6">
+                <div className="flex-none pl-6 text-md">
+                    <HeaderInfo header={header2} d1={d12} d2={d22} d3={d32}/>
                     {text.map((e) => 
                         <ul key={e}>
                             {e}
--- pages/gamedevelopment.tsx
+++ pages/gamedevelopment.tsx
@@ -3,7 +3,7 @@ import Gamedevinfo from "../components/gamedev/gamdevinfo";
 export default function GameDevelopment(){
     return(
         <div className="">
-            <Gamedevinfo linkPage="https://wolfeycs.itch.io/hullbreakers" imageSource="/headerCapsule.png" text={["Lead Game Designer", "Created an arcade space shooter game", "Worked closely with other game designers, producers", "and writers to bring the game's characters, stories, and worlds to life", "Space arcade game with fast paced combat, quick maneuvering, and strategy", "Use an ever-evolving spacecraft to wipe out endless hordes of enemies and", "relentless bosses in order to make your mark on the leaderboard"]} widthSize={900} heightSize={900}/>
+            <Gamedevinfo linkPage="https://wolfeycs.itch.io/hullbreakers" imageSource="/trailerLogo.png" text={["Lead Game Designer", "Created an arcade space shooter game", "Worked closely with other game designers, producers", "and writers to bring the game's characters, stories, and worlds to life", "Space arcade game with fast paced combat, quick maneuvering, and strategy", "Use an ever-evolving spacecraft to wipe out endless hordes of enemies and", "relentless bosses in order to make your mark on the leaderboard"]} widthSize={300} heightSize={300} header2="HullBreakers" d12="" d22="" d32=""/>
         </div>
     )
 }
