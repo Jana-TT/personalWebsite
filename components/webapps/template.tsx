c0bc03e3cc02ffee1a0fd9062310d98b8733a9be
Jana <janatt2056@gmail.com>
2022-12-23 11:55:30 -0600

    sofar

--- components/webapps/template.tsx
+++ components/webapps/template.tsx
@@ -1,42 +1,21 @@
-import Image from "next/image"
 import Link from "next/link"
+import Image from "next/image"
 
-interface props {
+interface props{
+    text: string
     imageSource: string
-    description: string[]
-    title: string
-    linkto: string
-    widthLength: number
-    heightLength: number
+    widthSize: number
+    heightSize: number
 }
 
-export default function Template( {imageSource, description, widthLength, heightLength, title, linkto}: props) {
+export default function Template({text, imageSource, widthSize, heightSize}: props) {
     return(
+        <button className="flex items-stretch justify-center">
 
-        <Link href={linkto} className="flex flex-row items-center pt-4">
+            <div className="flex">
+                <Image src={imageSource} alt="" width={widthSize} height={heightSize}/> 
+            </div>
 
-            <button className="bg-gray-300 flex hover:border rounded p-2 hover:bg-gray-200 border-blush hover:border-2">
-
-                <Image src={imageSource} alt={""} width={widthLength} height={heightLength}/>
-
-                <div className="pl-6 text-left">
-
-                    <div className="text-lg">
-                        {title}
-                    </div>
-
-                    <div className="font-serif text-md pt-4">
-                        {description.map((e) => 
-                        <li key={e}>
-                            {e}
-                        </li>
-                        )}
-                    </div>
-                    
-                </div> 
-
-            </button>
-
-        </Link>
+        </button>
     )
 }
--- pages/websites.tsx
+++ pages/websites.tsx
@@ -4,8 +4,7 @@ import Template from "../components/webapps/template";
 export default function Websites() {
     return(
         <div>
-            <Template linkto="https://4351-website.vercel.app/" imageSource="/opentable.png" title="OpenTable" description={["Created a reservation system for a restaurant", "Made with NextJS, Dgraph, Tailwind CSS", "Features:", "Takes in guests and registered users", "Users are able to search and make reservations", "Prompts users to register before finalizing a reservation", "System tracks high traffic days/weekends and displays a hold fee to the users"]} widthLength={800} heightLength={800}/>
-            <Template linkto="https://personalwebsite-hv5y.vercel.app/" imageSource="/myportfolio.png" title="Personal Portfolio" description={["Created a personal portfolio", "Made with NextJS, Tailwind CSS", "Features:", "Displays components that are typically found in a resume", "Includes education, skills, projects, work experience, and contact sections"]} widthLength={800} heightLength={800}/>
+            <Template text="jhgjkj" imageSource="/opentable.png" widthSize={900} heightSize={900}/>
         </div>
     )
 }
