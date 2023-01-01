0e943a12c94e2a5cc118369a8b6eca77bb63cf24
Jana <janatt2056@gmail.com>
2022-12-31 22:42:07 -0600

    yayy

--- pages/contactme.tsx
+++ pages/contactme.tsx
@@ -3,16 +3,16 @@ import Card from "../components/mainScreen/card";
 
 export default function ContactMe() {
     return(
-        <div className="mt-8 pt-56">
-            <div className="flex pb-6 justify-center">
-                <HeaderInfo header="Contact Me!" d1="" d2="" d3=""/>
-            </div>
+        <div className="mt-8 pt-56 grid grid-rows-2 items-center justify-center">
 
-            <div className="flex justify-center text-lg items-stretch space-x-6">
+            <HeaderInfo header="Contact Me!" d1="" d2="" d3=""/>
+
+            <div className="text-lg items-stretch space-x-6">
                 <Card pageLink="" title="Email" content={["janatt2056@gmail.com"]}/>  
                 <Card pageLink="" title="Phone Number" content={["+1 346-966-9608"]}/>  
                 <Card pageLink="https://github.com/Jana-TT" title="GitHub" content={["Jana-TT"]}/> 
             </div> 
+
         </div>
     )
 }
