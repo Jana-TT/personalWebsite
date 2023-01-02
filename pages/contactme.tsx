a78e6e4c8751aae864c418ebd96017511e903816
Jana-TT <112980645+Jana-TT@users.noreply.github.com>
2023-01-02 13:58:22 -0600

    stuffs added

--- pages/contactme.tsx
+++ pages/contactme.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 import HeaderInfo from "../components/animations/headerinfo";
 import Card from "../components/mainScreen/card";
+import Image from "next/image";
 
 export default function ContactMe() {
     return(
-        <div className="flex space-x-3 mt-32 justify-center">
-            eww
+        <div className="flex space-x-3 mt-32 justify-end pr-20">
+
+            <div className="w-1/2">
+                boop
+            </div>
+
+            <div>
+                
+            </div>
+
         </div>
     )
 }
