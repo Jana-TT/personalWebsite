61c852fafb8027acefac709ff1dd053f76557efb
Jana <janatt2056@gmail.com>
2023-01-04 19:01:03 -0600

    added

--- components/mainScreen/skills.tsx
+++ components/mainScreen/skills.tsx
@@ -4,7 +4,7 @@ export default function Skills() {
     return(
 
         <div className="flex space-x-3 mt-16 mx-auto">
-                <Card pageLink='' title='About Me!' content={["Honors graduate from the University of", "Houston seeking an entry-level position", "as a full stack web developer. Worked on", "group web projects, and made portfolio", "websites for consumers."]}/> 
+                <Card pageLink='' title='About Me!' content={["Honors graduate from the University of", "Houston seeking an entry-level position", "as a React full stack web developer.", "Worked on group web projects, and", "made portfolio websites for consumers."]}/> 
 
                 <Card pageLink='/resume' title='Skills' content={["Languages: JavaScript, C++, Python", "Front-End Framework: React", 
                 "Game Engine: Unity", "Databases: NoSQL (MongoDb), Dgraph", "Source Control: Git, PlasticSCM", "Operating System: Windows", "Tailwind CSS, CSS"]}/> 
