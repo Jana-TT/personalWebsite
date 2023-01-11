18cd77ecb5772ae68e4b2e537d1db46940d684b0
Jana <janatt2056@gmail.com>
2023-01-10 22:13:27 -0600

    kk

--- components/mainScreen/skills.tsx
+++ components/mainScreen/skills.tsx
@@ -4,14 +4,14 @@ export default function Skills() {
     return(
 
         <div className="flex space-x-3 mt-16 mx-auto">
-                <Card pageLink='' title='About Me!' content={["Honors graduate from the University of", "Houston seeking an entry-level position", "as a React full stack web developer.", "Worked on group web projects and", "made portfolio websites for consumers.", "Have worked with backend developers", "integrating frontend with PostgreSQL,", "MongoDB, Apollo/GraphQL, and Dgraph."]}/> 
+            <Card pageLink='' title='About Me!' content={["Honors graduate from the University of", "Houston seeking an entry-level position", "as a React full stack web developer.", "Detail-oriented, analytical, and self-driven", "programmer with extensive experience building user apps"]}/> 
 
                 <Card pageLink='/resume' title='Skills' content={["Languages: JavaScript, Typescript, C++, Python", "Front-End Framework: React", 
                 "Game Engine: Unity", "Databases: NoSQL (MongoDb), Dgraph", "Source Control: Git, PlasticSCM", "Operating System: Windows", "Tailwind CSS, CSS"]}/> 
 
                 <Card pageLink='gamedevelopment' title='Games' content={["2D Space Shooter: HullBreakers"]}/> 
 
-                <Card pageLink='websites' title='Web Apps' content={["Reservation System: OpenTable", "Personal Portfolio: this website"]}/> 
+                <Card pageLink='websites' title='Web Apps' content={["Reservation System: OpenTable", "Personal Portfolio: Jana Tahan", "Discord Inspired: Rick and Morty", "Business Client: Lateef Engineering"]}/> 
         </div>
  
     )
